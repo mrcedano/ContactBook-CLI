76121077b633cd4a8d57c651177efc1ef5b0a613
Mrcedano <oscarcedanob@gmail.com>
2024-08-11 21:22:56 -0700

    Adding a login

--- main.c
+++ main.c
@@ -1,10 +1,45 @@
 #include<stdio.h>
 #include"./app-logic/contact.c"
 
+#define USER "cedano"
+#define PASSWORD  "cedano"
+
+#define LOGIN_SUCESS 0
+#define LOGIN_FAILED 1
+
+int Login()
+{
+ char username[20];
+ char password[20];
+
+ puts("| Login |");
+ puts("Type your user: ");
+ scanf("%s", username); 
+ 
+ puts("Type your password");
+
+ scanf("%s", password);
+
+ if(strcmp(username, USER) == 0 && strcmp(password, PASSWORD) == 0)
+ {
+   return LOGIN_SUCESS;
+ }
+
+ return LOGIN_FAILED;
+}
+
 void PrintMenu();
 
 int main() 
 {
+
+ if(Login() == LOGIN_FAILED)
+ {
+   puts("Login unsucessful. Check your credentials");
+   return 1;
+ }
+
+ system("clear");
  int option = 0;
  
  struct ContactBook contactbook = InitContactBookSystem();
